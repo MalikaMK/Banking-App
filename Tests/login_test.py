2af5eeb9e100bbc583a4b095dc122098b76a62b0
Malika <malika86.mk@gmail.com>
2022-11-27 15:41:06 -0500

    Banking_App - new folder

---
+++ Tests/login_test.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from time import sleep
+from Pages.login_page import LoginPage
+
+def test_landing_page():
+    driver = webdriver.Firefox()
+    driver.get('https://demo.ebanq.com/log-in')
+    driver.maximize_window()
+    sleep(5)
+    assert driver.title == 'EBANQ'
+    driver.quit()
+    
+def test_user_login():
+    driver = webdriver.Firefox()
+    driver.get('https://demo.ebanq.com/log-in')
+    driver.maximize_window()
+    sleep(5)
+    log_page = LoginPage(driver)
+    log_page.user_login()
+    sleep(5)
+    assert 'Log Out' in driver.page_source
+    driver.quit()
+    
+    
+    
+    
